e513a7114b0860a78010db62e3332ae602191f4a
bejewelled <79288061+bejewelled@users.noreply.github.com>
2022-01-17 06:36:59 -0500

    stuff

---
+++ client/src/routes/game/gathering.svelte
@@ -0,0 +1,20 @@
+<script>
+import { onMount } from 'svelte';
+import {Container, Row, Col, NavLink, Nav, Navbar} from 'sveltestrap'
+
+
+</script>
+
+<main>
+    <div class='px-3 py-3 wrapper wrapper--gathering'>
+        lkjhkjgjhggjh
+    </div>
+</main> 
+
+<style>
+.wrapper--gathering {
+    margin-left: 225px;
+
+}
+
+</style>
