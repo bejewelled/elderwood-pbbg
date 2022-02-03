4b45540cf313273bbb17827553c681124c0d724f
bejewelled <jtg7sz@virginia.edu>
2022-02-02 20:25:14 -0500

    k

---
+++ server/app/Services/Math.ts
@@ -0,0 +1,16 @@
+// list of mathematical functions
+// AVOID LOOPS WHEN POSSIBLE. The goal is O(1), so use summation formulas when you can.
+
+class Math {
+
+    /**
+     * Arithmetic sum calculator where prices increase linearly
+     */
+    public arithmeticSum() {
+        
+    }
+
+
+}
+
+export default new Math();
